f3118772e5297b8015234837aee737b6fe93ba5e
Veestire <Veestire@gmail.com>
2018-01-14 21:36:31 +0100

    Revert "Adding a dm grabber to talking.py"
    This reverts commit d161161f76334b56314042ce7afacc78c8a77187.

--- cogs/talking.py
+++ cogs/talking.py
@@ -21,15 +21,12 @@ class Talking:
     async def say(self, ctx, channel_destination: str, *, inp):
         channel = ctx.message.channel_mentions[0]
         await self.bot.send_message(channel, inp)
-
+		
     @commands.has_permissions(administrator=True)
     @commands.command(hidden=True, pass_context=True)
     async def dm(self, ctx, channel_destination: str, *, inp):
         dest = ctx.message.mentions[0]
-        dest2 = self.bot.get_channel('402196014485733387')
         await self.bot.send_message(dest, inp)
-        await self.bot.send_message(dest2, inp)
-
 
 def setup(bot):
     bot.add_cog(Talking(bot))
