a663e5626db5e754d6a271d170a8e4a452d1f440
js6pak <kubastaron@hotmail.com>
2018-01-16 16:30:01 +0100

    Fixing "" in translate

--- cogs/translate.py
+++ cogs/translate.py
@@ -13,7 +13,7 @@ class Translate:
 	
 
 	@commands.command()
-	async def translate(self, text=[]):
+	async def translate(self, text: str):
 		translations = translator.translate(text, src='en', dest='pl')
 		out : str
 		for translation in translations:
@@ -21,7 +21,7 @@ class Translate:
 		await self.bot.say(out)
 
 	@commands.command()
-	async def tlumacz(self, text=[]):
+	async def tlumacz(self, text: str):
 		translations = translator.translate(text, src='pl', dest='en')
 		out : str
 		for translation in translations:
