dba70fe2f500c5a3b753e0eb0fc6cf152a7dc2ea
js6pak <kubastaron@hotmail.com>
2018-01-16 14:47:11 +0100

    Added translate.py

---
+++ cogs/translate.py
@@ -0,0 +1,27 @@
+from google.cloud import translate
+import discord
+from discord.ext import commands
+from discord.ext.commands import cooldown
+from sys import argv
+
+class Translate:
+		def __init__(self, bot):
+	        self.bot = bot
+	        print('Addon "{}" loaded'.format(self.__class__.__name__))
+	# Instantiates a client
+	translate_client = translate.Client()
+
+	    @commands.command()
+	    async def translate(self, ctx, text : str):
+	    	target = 'pl'
+	    	translation = translate_client.translate(text,target_language=target)
+	    	await self.bot.say(translation['translatedText'])
+
+	    @commands.command()
+	    async def tlumacz(self, ctx, text : str):
+	    	target = 'en'
+	    	translation = translate_client.translate(text,target_language=target)
+	    	await self.bot.say(translation['translatedText'])
+
+def setup(bot):
+	bot.add_cog(Translate(bot))
