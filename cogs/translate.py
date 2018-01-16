df7b61fe245d7cdcfbb5519f3f7652fe3fb34555
js6pak <kubastaron@hotmail.com>
2018-01-16 22:50:01 +0100

    "Fixed" emoji fatal error

--- cogs/translate.py
+++ cogs/translate.py
@@ -14,13 +14,20 @@ class Translate:
 
 	@commands.command()
 	async def translate(ctx, *, arg):
-		out = translator.translate(arg, src='en', dest='pl').text
-		await self.bot.say(out)
+		try:
+			out = translator.translate(arg, src='en', dest='pl').text
+			await ctx.bot.say(out)
+		except:
+			await ctx.bot.say("Error while translating try remove emoji (Emoji support will be added soon)")
 
 	@commands.command()
 	async def tłumacz(ctx, *, arg):
-		out = translator.translate(arg, src='pl', dest='en').text
-		await ctx.bot.say(out)
+		try:
+			out = translator.translate(arg, src='pl', dest='en').text
+			await ctx.bot.say(out)
+		except:
+			await ctx.bot.say("Problem z tłumaczeniem spróbuj usunąć emoji (Wsparcie dla emoji zostanie wkrótce dodane)")
+		
 
 def setup(bot):
 	bot.add_cog(Translate(bot))
